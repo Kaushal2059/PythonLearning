58a12a7e6bea67ca72ce50b47471e77a7776b492
Kaushal20 <rupakhetikaushal7@gmail.com>
2025-06-28 08:44:46 +0545

    new sissor paper game started.

--- Randoization/RockPapergame.py
+++ Randoization/RockPapergame.py
@@ -1,4 +1,21 @@
+import random
 print("welcome to the rock paper sizzor game")
 
-input_value = input("Enter what you want to choose between rock paper and sissor").lower()
+input_value = int(input("Enter what you want to choose? Enter 0 for rock 1 for paper and 2 for sissor"))
 
+computer_choice = random.randint(0,2)
+print(f"The computer choose {computer_choice}")
+
+if input_value == 0 and computer_choice == 2:
+    print("congratulations you won! ")
+elif computer_choice == 0 and input_value == 2:
+    print("Opps! you lost against the computer.")
+elif input_value > computer_choice:
+    print("congratulations you won!")
+elif computer_choice > input_value:
+    print("Opps! you lost against the computer.")
+elif computer_choice == input_value:
+    print("Its a draw. Try again for the reasult hopefully")
+else:
+    print("You typed an invalid number. You losed!")
+    
