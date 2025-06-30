356ea763f42513d2e9648e91ffe276d41f89b1cc
Kaushal20 <rupakhetikaushal7@gmail.com>
2025-06-30 20:56:25 +0545

    further for loop

---
+++ Loops/Maximumnum.py
@@ -0,0 +1,12 @@
+student_scores = [50, 100, 25, 220, 500, 60, 330, 550, 80]
+
+max_num = 0
+for marks in student_scores:
+    if marks > max_num:
+        max_num = marks
+print(f"{max_num} is the maximum number")        
+        
+        
+    
+         
+       
