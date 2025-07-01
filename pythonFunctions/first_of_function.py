31719d3ef6765fbc4ba314b956bbae6e296e08b1
Kaushal20 <rupakhetikaushal7@gmail.com>
2025-07-01 18:40:45 +0545

    beggining of python functions

---
+++ pythonFunctions/first_of_function.py
@@ -0,0 +1,6 @@
+def new_function():  # def defines the name of the function and paranthesis differs functions from a variable name. and : denotes anything after it is a part of that function followed by indentation
+    print("hello")
+    print("Bye")
+
+new_function()  # this way you can call the function
+    
